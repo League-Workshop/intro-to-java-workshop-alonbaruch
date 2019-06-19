8cf0b38751274696af67a1df33045aec8d316020
League Student <league@iMac-26.attlocal.net>
2019-06-19 14:28:13 -0700

    Don't be a sweaty tryhard!

--- src/section2/FlamingNinjaStar.java
+++ src/section2/FlamingNinjaStar.java
@@ -1,5 +1,7 @@
 package section2;
 
+import java.awt.Color;
+
 import org.jointheleague.graphical.robot.Robot;
 
 public class FlamingNinjaStar {
@@ -11,34 +13,37 @@ public class FlamingNinjaStar {
 		
 		// 1. Make a new robot, and set it's pen down.
 		Robot fortnite=new Robot("batman");
+		fortnite.penDown();
 		// *14. Use the methods setX and setY to move the ninja star into the center of the screen
 		
 		// *15. Make some adjustments to see what other kinds of shapes you can make.
 
 		// 12. Set the robot speed to 100
-
+		fortnite.setSpeed(100);
 		// 13. Use a for loop to repeat all of the code below 25 times
+for (int i = 0; i < 25; i++) {
+	
 
 			// 2. Turn the robot 1/8 of a circle
-		
+		fortnite.turn(1/8);
 			// 3. Move the robot 64 pixels
-
+		fortnite.move(64);
 			// 4. Turn the robot 40 degrees to the LEFT. (Negative numbers will turn the robot counter-clockwise.)
-			
+		fortnite.turn(-40);
 			// 5. Move the robot the distance in the variable flameSize
-			
+		fortnite.move(flameSize);
 			// 6. Turn the robot 170 degrees
-			
+		fortnite.turn(170);
 			// 7. Move the robot the distance in the variable flameSize (again)
-			
+		fortnite.move(flameSize);
 			// 8. Turn the robot 64 degrees to the right
-			
+		fortnite.turn(64);
 			// 9. Move the robot the distance in the variable baseSize
-			
+		fortnite.move(baseSize);
 			// 10. Check that your shape is the same as Figure 1. This is one arm of the ninja star.
 			
 			// 11. Color your ninja star like Figure 2.
-		
+		fortnite.setPenColor(Color.GRAY);}
 	}
 
 }
--- src/section3/CandyCrush.java
+++ src/section3/CandyCrush.java
@@ -6,13 +6,14 @@ public class CandyCrush {
 
 	public static void main(String[] args) {
 		// 1. Using a pop-up, ask the user who they don't like
-
+String sweatySoccerSkin=JOptionPane.showInputDialog(null, "who don't like!");
 		// 2. Change the next line so that the pop-up will tell them they have a crush on that person
-		JOptionPane.showMessageDialog(null, "You totally have a crush on Java!");
+		JOptionPane.showMessageDialog(null, "You totally have a crush on "+ sweatySoccerSkin);
 		// 3. Ask the user for the name of their best friend
-
+		JOptionPane.showMessageDialog(null, "You totally have a crush on "+ sweatySoccerSkin);
 		// 4. Tell them their best friend is as sweet as candy
-
+		String frenchNumber69SweatySoccerSkin=JOptionPane.showInputDialog(null, "who is your best friend");
+		JOptionPane.showMessageDialog(null,frenchNumber69SweatySoccerSkin+" is as sweet as candy");
 	} 
 }
 
