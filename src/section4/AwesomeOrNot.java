d7dec7442ecda0a2b3add923fcdc3baf11f09221
League Student <league@iMac-26.attlocal.net>
2019-06-21 14:25:52 -0700

    we did quiz game and awsome or not

--- src/section4/AwesomeOrNot.java
+++ src/section4/AwesomeOrNot.java
@@ -2,13 +2,18 @@ package section4;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class AwesomeOrNot {
 
 	// 1. Make a main method that includes all the steps below…
+public static void main(String[] args) {
+	
 
 	// 2. Make a variable that will hold a random number and put a random number into this variable using "new Random().nextInt(4)"
-
+int one = new Random() .nextInt(4);
 	// 3. Print out this variable
+System.out.println(one);
 
 	// 4. Get the user to enter something that they think is awesome
 
@@ -28,6 +33,6 @@ public class AwesomeOrNot {
 
 	// -- write your own answer
 
-}
+}}
 
 
--- src/section4/QuizGame.java
+++ src/section4/QuizGame.java
@@ -1,20 +1,29 @@
 package section4;
 
+import javax.swing.JOptionPane;
+
 public class QuizGame {
 	
 	public static void main(String[] args) {
 		
 		// 1.  Create a variable to hold the user's score 
+		int number = 0; 
 		
 		// 2.  Ask the user a question 
-		
+		String a = JOptionPane.showInputDialog(null,"What is 789*10");
 		// 3.  Use an if statement to check if their answer is correct
+		if ("7890".equals(a)) { number += 1;}
+			
 		
 		// 4.  if the user's answer was correct, add one to their score 
-		
+	
 		// 5.  Create more questions by repeating steps 1, 2, and 3 below. 
-		
+		String b = JOptionPane.showInputDialog(null,"X=12 Y=2 X*(Y*Y)=");
+		if("48".equals(b)) { number += 2;}
 		// 6.  After all the questions have been asked, print the user's score 
-		
+		String d = JOptionPane.showInputDialog(null,"x=12 z=2 y=5 x*((y*y)*(z*z))=");
+		if ("480".equals(d)) { number += 3;}
+
+System.out.println(number);
 	}
 }
